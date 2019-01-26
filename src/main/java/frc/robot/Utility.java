6a3dd91dae60f5688f10b91c296dbdd6b840a66d
Will Leverone <Barrelwolf38087@gmail.com>
2019-01-26 12:28:35 -0500

    Add some Javadoc.

--- src/main/java/frc/robot/Utility.java
+++ src/main/java/frc/robot/Utility.java
@@ -15,6 +15,14 @@ public class Utility {
 		return result;
 	}
 	
+	/**
+	 * Limit a value to being between two boundaries.
+	 * @param testValue The value we want to look at.
+	 * @param lowerBound The lowest {@code testValue} should be able to go.
+	 * @param upperBound The highest {@code testValue} should be able to go.
+	 * @return {@code testValue} if it falls between {@code lowerBound} and {@code upperBound},
+	 *         or the value of the boundary it crosses.
+	 */
 	public static double limitValue(double testValue, double lowerBound, double upperBound)
 	{
 		if(testValue > upperBound)
