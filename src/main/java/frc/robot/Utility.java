0ed262bacfda8e53cf0279c936b9f8f5d506cb5f
Will Leverone <Barrelwolf38087@gmail.com>
2019-01-26 13:45:17 -0500

    Remove unused method diffAngles

--- src/main/java/frc/robot/Utility.java
+++ src/main/java/frc/robot/Utility.java
@@ -26,19 +26,6 @@ public class Utility {
 			return testValue;
 		}
 	}
-
-
-	
-	public static double diffAngles(double angle1, double angle2) {
-		// returns unwrapped difference between two wrapped angles
-		// angles are assumed to wrap at +/-180 degree boundary
-		// result = angle1-angle2
-		double result=0;
-		result=angle1-angle2;
-		if (result<-180) result+=360;
-		if (result>180) result-=360;
-		return result;
-	}
 	
 	public static double applyDeadZone(double speed)
 	{
