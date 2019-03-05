140b9e9cb77d92570e433406abf65014577bbbad
Liam Daley <liamdaley276@gmail.com>
2019-03-04 19:24:47 -0500

    More ManipulatorSequential

--- src/main/java/frc/robot/commands/groups/ManipulatorSequential.java
+++ src/main/java/frc/robot/commands/groups/ManipulatorSequential.java
@@ -1,21 +1,32 @@
 package frc.robot.commands.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Robot;
 import frc.robot.commands.Actuate;
 import frc.robot.commands.ElevateToTarget;
+import frc.robot.subsystems.Manipulator;
 import frc.robot.subsystems.Elevator.ElevatorTarget;
 
 public class ManipulatorSequential extends CommandGroup {
 
     public ManipulatorSequential (boolean tra, boolean rot, ElevatorTarget tar) {
+        requires(Robot.sequenceCoordinator);
 
-        if (tra != false) {
-            addSequential(new Actuate(tra, rot));
-        }
+        addSequential(new Actuate(tra, rot));
 
-        if (tar != ElevatorTarget.NONE) {
-            addSequential(new ElevateToTarget(tar));
-        }
+        addSequential(new ElevateToTarget(tar));
+    }
+
+    public ManipulatorSequential (boolean tra, boolean rot) {
+        requires(Robot.sequenceCoordinator);
+
+        addSequential(new Actuate(tra, rot));
+    }
+
+    public ManipulatorSequential (ElevatorTarget tar) {
+        requires(Robot.sequenceCoordinator);
+        
+        addSequential(new ElevateToTarget(tar));
     }
 
 }
