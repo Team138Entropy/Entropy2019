df6271869ee9afa9d6ee5f03f4572ba2a4915e35
maxh65000 <booah8@gmail.com>
2019-06-02 19:48:52 -0400

    Revert "do the new things"
    This reverts commit ca664dd421bd5b4e7708f194d0c340fa0722adf9.

--- src/main/java/frc/robot/commands/DeployCargo.java
+++ src/main/java/frc/robot/commands/DeployCargo.java
@@ -1,23 +1,15 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.subsystems.AcquisitionRoller;
 import frc.robot.Constants;
-import frc.robot.Robot;
-import frc.robot.subsystems.Elevator.ElevatorTarget;
 
 public class DeployCargo extends CommandGroup {
 
     public DeployCargo() {
-        requires(Robot.elevator);
-
-        boolean isLevel1 = Robot.elevator.getTargetPosition() == ElevatorTarget.LEVEL_1;
-        boolean manipulatorState = isLevel1 ? Constants.HORIZONTAL : Constants.VERTICAL;
-        Command rollerState = isLevel1 ? new ExtendRoller() : new RetractRoller();
-
-        addSequential(new RotateCargoManipulator(manipulatorState));
+        addSequential(new RotateCargoManipulator(Constants.HORIZONTAL));
         addSequential(new Wait(Constants.CARGO_LOWER_DELAY));
-        addSequential(rollerState);
+        addSequential(new RetractRoller());
         addSequential(new Wait(Constants.PNEUMATIC_DELAY));
     }
 }
