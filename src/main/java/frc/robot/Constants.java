83868a2dfd4021a2c012aaa042b8924194f7e42b
maxh65000 <booah8@gmail.com>
2019-06-02 20:01:45 -0400

    Reduced HATCH_PANEL_DOWN

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -77,7 +77,7 @@ public class Constants {
 	public static final double PNEUMATIC_DELAY = 0.5;
 	public static final double CARGO_LOWER_DELAY = 1.0;
 	public static final int HATCH_PANEL_UP = 150; // TODO: Fine Tune
-	public static final int HATCH_PANEL_DOWN = -100; // TODO: Fine Tune
+	public static final int HATCH_PANEL_DOWN = -80; // TODO: Fine Tune
 
 
 	// Climber 
